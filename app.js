6a51b07fc294b83c8e3262130e22e09f382447a3
bolheinmin <bolheinmin@gmial.com>
2020-11-03 14:49:53 +0630

    almost final

--- app.js
+++ app.js
@@ -993,11 +993,17 @@ const saveBuyerAppointment = (arg, sender_psid) => {
         console.log('SAVED', success);
         let text = "Thank you. We have received your appointment." + "\u000A";
         text += " We wil call you to confirm soon" + "\u000A";
-        text += "Your booking reference number is:" + data.ref;
+        text += "Your booking reference number is:";
+        let refNo = data.ref;
         let response = {
             "text": text
         };
-        callSend(sender_psid, response);
+        let response2 = {
+            "text": refNo
+        }
+        callSend(sender_psid, response1).then(() => {
+            return callSend(sender_psid, response2);
+        });
     }).catch((err) => {
         console.log('Error', err);
     });
